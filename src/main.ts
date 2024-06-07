b392cbb8f672663550e69d4b0646bc924d1a6c3e
luoluoqixi <luoqixi@luoqixi.com>
2024-06-07 23:12:53 +0800

    remove example

---
+++ src/main.ts
@@ -0,0 +1,75 @@
+import { App, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+// Remember to rename these classes and interfaces!
+
+interface HomeFileBrowserPluginSettings {
+	mySetting: string;
+}
+
+const DEFAULT_SETTINGS: HomeFileBrowserPluginSettings = {
+	mySetting: 'default'
+}
+
+export default class HomeFileBrowserPlugin extends Plugin {
+	settings: HomeFileBrowserPluginSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+			new Notice('This is a notice!');
+		});
+		// Perform additional things with the ribbon
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new HomeFileBrowserPluginSettingTab(this.app, this));
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			console.log('click', evt);
+		});
+
+		// // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class HomeFileBrowserPluginSettingTab extends PluginSettingTab {
+	plugin: HomeFileBrowserPlugin;
+
+	constructor(app: App, plugin: HomeFileBrowserPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
